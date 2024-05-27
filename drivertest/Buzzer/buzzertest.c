40ab3291d2bfbde736b52e8995963449a4bca799
SeoyoungJS2 <169055565+SeoyoungJS2@users.noreply.github.com>
2024-05-28 05:18:49 +0900

    Update buzzertest.c

--- drivertest/Buzzer/buzzertest.c
+++ drivertest/Buzzer/buzzertest.c
@@ -13,10 +13,10 @@ int main(void)
    printf("BGM_START: GAME START!\r\n");
    BGM_START();
 
-   printf("BGM_GAME1: 미로 GAME START!\r\n");
+   printf("BGM_GAME1: GAME START!\r\n");
    BGM_GAME1();
 
-   printf("BGM_GAME2: 같은 그림 찾기 GAME START!\r\n");
+   printf("BGM_GAME2: GAME START!\r\n");
    BGM_GAME2();
 
    printf("buzzer off\r\n");
