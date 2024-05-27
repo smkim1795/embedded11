9f2bf473fdc68428e6264feeba66e6fa13dab044
SeoyoungJS2 <169055565+SeoyoungJS2@users.noreply.github.com>
2024-05-28 03:13:23 +0900

    Update BGM

--- drivertest/Buzzer/buzzer.c
+++ drivertest/Buzzer/buzzer.c
@@ -12,16 +12,17 @@
 #define BUZZER_ENABLE_NAME "enable"
 #define BUZZER_FREQUENCY_NAME "frequency"
 
-char gBuzzerBaseSysDir[128];
-const int musicScale[MAX_SCALE_STEP] =
+static char gBuzzerBaseSysDir[128];
+static const int musicScale[MAX_SCALE_STEP] =
 {
-   262, /*do*/ 294,330,349,392,440,494, /* si */ 523, 329, 350, 247, 213, 262, 165, 174, 196, 220
-//  0           1   2   3   4   5   6            7    8    9   10(시) 11   12  13(미)14(파)15(솔)16(라)
+   262, /*do*/ 294,330,349,392,440,494, /* si */ 523
+//  0           1   2   3   4   5   6            7
 };
 
-int freIndex;
+static int fd_en, fd_fre;
+static char path_en[200] = {0,}, path_freq[200] = {0,};
 
-int findBuzzerSysPath() //버저 경로 찾기
+int findBuzzerSysPath(char *gBuzzerBaseSysDir) //버저 경로 찾기
 {
    DIR * dir_info = opendir(BUZZER_BASE_SYS_PATH);
    int ifNotFound = 1;
@@ -30,26 +31,17 @@ int findBuzzerSysPath() //버저 경로 찾기
          struct dirent *dir_entry;
          dir_entry = readdir (dir_info);
          if (dir_entry == NULL) break;
-         if (strncasecmp(BUZZER_FILENAME, dir_entry->d_name, strlen(BUZZER_FILENAME)) == 0){
+         if (strncasecmp(BUZZER_FILENAME, dir_entry->d_name, strlen(BUZZER_FILENAME)) == 0)
+         {
             ifNotFound = 0;
             sprintf(gBuzzerBaseSysDir,"%s%s/",BUZZER_BASE_SYS_PATH,dir_entry->d_name);
-            }
+         }
       }
    }
 
    printf("find %s\n",gBuzzerBaseSysDir);
-
    return ifNotFound;
 }
-
-/*void doHelp(void)
-{
-   printf("Usage:\n");
-   printf("buzzertest <buzzerNo> \n");
-   printf("buzzerNo: \n");
-   printf("do(1),re(2),mi(3),fa(4),sol(5),ra(6),si(7),do(8) \n");
-   printf("off(0)\n");
-}*/
 
 void buzzerEnable(int bEnable)
 {
@@ -70,173 +62,166 @@ void setFrequency(int frequency)
    close(fd);
 }
 
-int buzzerInit(void)
+int buzzerInit(void) //+Buzzerenable, setFrequency (sprintf)
 {
-   if(freIndex > MAX_SCALE_STEP)
+   if(findBuzzerSysPath(gBuzzerBaseSysDir) == 1)
    {
-      printf(" <buzzerNo> over range \n");
-      doHelp();
-      return 1;
+      printf("Failed. File not exist\r\n");
+      return 0;
    }
-   return freIndex;
+   
+   sprintf(path_en,"%s%s",gBuzzerBaseSysDir,BUZZER_ENABLE_NAME);
+   sprintf(path_fre,"%s%s",gBuzzerBaseSysDir,BUZZER_FREQUENCY_NAME);
+
+   fd_en=open(path,O_WRONLY);
+   fd_fre=open(path,O_WRONLY);
+
+   return 1;
 }
 
-int buzzerPlaySong(int freIndex)
+int buzzerPlaySong(int scale)
 {
-   if(freIndex > 0 || freIndex < MAX_SCALE_STEP)
-   {
-      setFrequency(musicScale[freIndex-1]);
-      buzzerEnable(1);
-   }
+   dprintf(fd_fre, "%d", scale);
+   write(fd_en, &"1", 1);
+
+   return 1;
 }
 
 int buzzerStopSong(void)
 {
- 
-   if ( freIndex == 0)
-   {
-      buzzerEnable(0);
-   }
+   write(fd_en, &"0", 1);
+
+   return 1;
 }
 
 int buzzerExit(void)
 {
    buzzerStopSong();
-   close(fd);
+   close(fd_fre);
+   close(fd_en);
    return 0;
 }
 
-int BGM1(void)
+int BGM_START(void)
+{   
+   buzzerPlaySong(musicScale[0]);
+   usleep(150000);
+   buzzerPlaySong(musicScale[2]);
+   usleep(150000);
+   buzzerPlaySong(musicScale[4]);
+   usleep(150000);
+   buzzerPlaySong(musicScale[7]); 
+   usleep(450000); //도미솔도
+   
+   buzzerStopSong(); // 부저 끄기
+}
+
+int BGM_GAME1(void) //도미솔 도미솔 라라라 솔 파파파 미미미 레레레 도 (미로 전용 BGM)
 {
-    buzzerPlaySong(musicScale[4]);
-    usleep(300000);
-    buzzerPlaySong(musicScale[8]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[9]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[4]);
-    usleep(300000);
-    buzzerPlaySong(musicScale[8]); 
-    usleep(150000);  //5 392, 329, 350, 392, 329, //5
-
-    buzzerPlaySong(musicScale[9]); 
-    usleep(150000);
-    buzzerPlaySong(musicScale[4]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[11]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[10]); 
-    usleep(150000); //10  350, 392, 245, 213, 245, //10
-
-    buzzerPlaySong(musicScale[12]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[1]);
+    buzzerPlaySong(musicScale[0]); 
     usleep(150000);
     buzzerPlaySong(musicScale[2]);
     usleep(150000);
-    buzzerPlaySong(musicScale[3]); 
+    buzzerPlaySong(musicScale[4]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[0]);
     usleep(150000);
     buzzerPlaySong(musicScale[2]); 
-    usleep(300000); //15 262, 294, 330, 349, 330, //15
+    usleep(150000);  
+    buzzerPlaySong(musicScale[4]); 
+    usleep(150000); //도미솔도미솔
+    buzzerPlaySong(musicScale[5]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[5]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[5]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[4]); 
+    usleep(450000); //라라라솔
 
-    buzzerPlaySong(musicScale[12]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[1]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[2]);
-    usleep(300000);
-
-
-    buzzerPlaySong(musicScale[13]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[14]);
-    usleep(150000); //20 262, 294, 330, 110, 130, //20
-
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[14]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000); //25 173, 221, 173, 130, 173, //25
-
-
-
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(300000);
-
-
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000); //30 262, 245, 262, 221, 262, //30
-
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(300000); 
-
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[14]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000); //35 245, 221, 173, 130, 173, //35
-
-    buzzerPlaySong(musicScale[14]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[13]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[14]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[15]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(150000); //40 130, 110, 130, 173, 221, //40
-
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(300000);
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000); //45 245, 262, 221, 262, 245, //45
-
-    buzzerPlaySong(musicScale[0]);
-    usleep(300000);
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[16]);
-    usleep(150000); //50 262, 245, 262, 245, 221, //50
-
-    buzzerPlaySong(musicScale[10]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[0]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[1]);
-    usleep(150000);
-    buzzerPlaySong(musicScale[2]);
+    buzzerPlaySong(musicScale[3]);
     usleep(150000);
     buzzerPlaySong(musicScale[3]);
-    usleep(150000); //55 245, 262, 294, 330, 349, //55
+    usleep(150000);
+    buzzerPlaySong(musicScale[3]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]); 
+    usleep(150000);
+    buzzerPlaySong(musicScale[2]);
+    usleep(150000); //파파파미미미
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[0]);
+    usleep(450000); //레레레도
 
+    buzzerStopSong(); // 부저 끄기
+}
+
+int BGM_GAME2(void) //솔미미 파레레 도레미파 솔솔솔 솔미미미 파레레 도미솔솔 미미미(같은그림찾기 전용 BGM))
+{
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);//솔미미
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);//파레레
+
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[7]);
+    usleep(300000);//도레미파솔솔솔
+
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
     buzzerPlaySong(musicScale[4]);
-    usleep(150000); 
+    usleep(300000);//솔미미미 파레레
+
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[1]);
+    usleep(150000);
+    buzzerPlaySong(musicScale[4]);
+    usleep(300000);//도미솔솔 미미미
+
     buzzerStopSong(); // 부저 끄기
 }
