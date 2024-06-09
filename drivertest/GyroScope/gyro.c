d0f3a1ced6b0c776e3b3af5dae27cc5cdb7c06e8
MoonScott <162456511+MoonScott@users.noreply.github.com>
2024-06-09 21:20:42 +0900

    Update gyro.c

--- drivertest/GyroScope/gyro.c
+++ drivertest/GyroScope/gyro.c
@@ -2,42 +2,52 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "gyro.h"
 
-#define ACCELPATH "/sys/class/misc/FreescaleAccelerometer/"
-#define MAGNEPATH "/sys/class/misc/FreescaleMagnetometer/"
-#define GYROPATH "/sys/class/misc/FreescaleGyroscope/"
+#define ACCEL_THRESHOLD 5000
+#define ACCEL_Z_THRESHOLD 9000
+#define accel_t 500000
 
-int gyro_options(void){
-    int fd = 0;
-    FILE *fp = NULL;
+int main(void) {
+    int first_accel[3];
+    int second_accel[3];
 
-    //Accelerometer
-    fd = open (ACCELPATH "enable", O_WRONLY);
-    dprintf(fd,"1");
-    close(fd);
-    fp = fopen(ACCELPATH "data", "rt");
-    int accel[3];
-    fscanf(fp, "%d, %d, %d", &accel[0], &accel[1], &accel[2]);
-    printf("I read Accel %d, %d, %d\r\n", accel[0], accel[1], accel[2]);
-    fclose(fp);
+    printf("Move Player");
+    getAccel(first_accel);
 
-    //Magnetometer
-    fd = open (MAGNEPATH "enable", O_WRONLY);
-    dprintf(fd,"1");
-    close(fd);
-    fp = fopen(MAGNEPATH "data", "rt");
-    int magne[3];
-    fscanf(fp, "%d, %d, %d", &magne[0], &magne[1], &magne[2]);
-    printf("I read Magneto %d, %d, %d\r\n", magne[0], accel[1], accel[2]);
-    fclose(fp);
+    while (1) {
+        getAccel(second_accel);
 
-     //Gyroscope
-    fd = open (GYROPATH "enable", O_WRONLY);
-    dprintf(fd,"1");
-    close(fd);
-    fp = fopen(GYROPATH "data", "rt");
-    int gyro[3];
-    fscanf(fp, "%d, %d, %d", &gyro[0], &gyro[1], &gyro[2]);
-    printf("I read Gyroscope %d, %d, %d\r\n", gyro[0], gyro[1], gyro[2]);
-    fclose(fp);
+        if (first_accel[0] - second_accel[0] > ACCEL_THRESHOLD)
+        {
+            printf("Moving Right\n");
+        }
+        else if (second_accel[0] - first_accel[0] > ACCEL_THRESHOLD)
+        {
+            printf("Moving Left\n");
+        }
+        else if (first_accel[2] - second_accel[2] > ACCEL_Z_THRESHOLD)
+        {
+            printf("Moving Backward\n");
+        }
+        else if (second_accel[2] - first_accel[2] > ACCEL_THRESHOLD)
+        {
+            if (second_accel[2] - first_accel[2] < ACCEL_Z_THRESHOLD)
+            {
+                printf("Moving Forward\n");
+            }
+            else
+            {
+                printf("Accelerating Forward\n");
+            }
+        }
+        else
+        {
+            printf("Stable\n");
+        }
+
+        usleep(accel_t); // wait for the next measurement
+    }
+
+    return 0;
 }
