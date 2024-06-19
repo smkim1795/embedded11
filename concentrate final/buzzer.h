a49337015d4fd4ef7b7b5ab9291d2e84c5427ad4
hyeonbin1 <db173328@naver.com>
2024-06-20 03:38:53 +0900

    buzzer.h

---
+++ concentrate final/buzzer.h
@@ -0,0 +1,10 @@
+#ifndef _BUZZER_H_
+#define _BUZZER_H_
+int buzzerInit(void);
+int buzzerPlaySong(int scale);
+int buzzerStopSong(void);
+int buzzerExit(void);
+int BGM_STRAT(void);
+int BGM_GAME1(void);
+int BGM_GAME2(void);
+#endif
