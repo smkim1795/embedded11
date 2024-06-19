6be307997fe8311701a2306370ed29024c4659d2
hyeonbin1 <db173328@naver.com>
2024-06-20 03:40:52 +0900

    colorled.h

---
+++ concentrate final/colorled.h
@@ -0,0 +1,26 @@
+colorled.h
+
+
+#ifndef _COLOR_H_
+
+#define _COLOR_H_
+
+
+
+int pwmActiveAll();
+
+int pwmInactiveAll();
+
+int pwmSetDuty(int dutyCycle, int pwmIndex);
+
+int pwmSetPeriod(int Period, int pwmIndex);
+
+int pwmSetPercent(int percent, int ledColor);
+
+int pwmStartAll();
+
+int pwmLedInit();
+
+
+
+#endif
