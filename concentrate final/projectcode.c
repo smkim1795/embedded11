e25cc139df3fe87dcb5fbbf149e214f2d204f866
hyeonbin1 <db173328@naver.com>
2024-06-20 03:07:13 +0900

    수정

--- concentrate final/projectcode.c
+++ concentrate final/projectcode.c
@@ -134,7 +134,7 @@ void *thread_object_1()
 
             while (stage1_end == 0)
             {
-                read_bmp("red.bmp", &data, &cols, &rows); // 미로 게임에서 사용되는 선 긋는 용 bmp 출력
+                read_bmp("player.bmp", &data, &cols, &rows); // 미로 게임에서 사용되는 선 긋는 용 bmp 출력
                 fb_write_c(data, cols, rows, new_x, new_y);
 
                 accel_x = get_accel_x(); // 가속도 센서 값 불러옴
@@ -162,41 +162,41 @@ void *thread_object_1()
                     new_x = new_x - 10;
                 }
 
-                read_bmp("red.bmp", &data, &cols, &rows); // 이동하는 aim의 새로운 좌표를 업데이트하여 다시 출력
+                read_bmp("player.bmp", &data, &cols, &rows); // 이동하는 aim의 새로운 좌표를 업데이트하여 다시 출력
                 fb_write_c(data, cols, rows, new_x, new_y);
                 printf("x: %d       y: %d\n\n", new_y, new_x); // aim의 실시간 좌표 확인
 
                 if ((new_x >= 130 && new_x <= 390) && (new_y == 370))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
                 } //1번선
                 else if ((new_y >= 450 && new_y <= 510) && (new_x == 270))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
                 } //2번선
                 else if ((new_y >= 450 && new_y <= 510) && (new_x == 400))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
                 } //3번선
                 else if ((new_x >= 400 && new_x <= 670) && (new_y == 450))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
                 } //4번선
                 else if ((new_x >= 550 && new_x <= 670) && (new_y == 370))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -204,7 +204,7 @@ void *thread_object_1()
 
                 else if ((new_y >= 220 && new_y <= 430) && (new_x == 810))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -212,7 +212,7 @@ void *thread_object_1()
 
                 else if ((new_y >= 220 && new_y <= 270) && (new_x == 700))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -220,7 +220,7 @@ void *thread_object_1()
 
                 else if ((new_x >= 550 && new_x <= 810) && (new_y == 150))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -228,7 +228,7 @@ void *thread_object_1()
 
                 else if ((new_x >= 270 && new_x <= 670) && (new_y == 190))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -236,7 +236,7 @@ void *thread_object_1()
 
                 else if ((new_y >= 70 && new_y <= 190) && (new_x == 270))
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -244,7 +244,7 @@ void *thread_object_1()
 
                 else if ((new_y >= 70 && new_y <= 110) && new_x == 370)
                 {
-                    read_bmp("output.bmp", &data, &cols, &rows);
+                    read_bmp("fail.bmp", &data, &cols, &rows);
                     sleep(2);
                     fail = 1;
                     break;
@@ -336,7 +336,7 @@ int main(void)
         printf("FrameBuffer Init Failed\r\n"); // TFT LCD 초기화
     }
 
-    read_bmp("output.bmp", &data, &cols, &rows); // 게임 시작 초기 화면 :: STRAT PAGE
+    read_bmp("start.bmp", &data, &cols, &rows); // 게임 시작 초기 화면 :: STRAT PAGE
     fb_write(data, cols, rows);
     system("sudo amixer sset 'Speaker' 70%");
     system("sudo aplay ./startbgm.wav");
@@ -375,7 +375,7 @@ int main(void)
     BGM_START();
 
     // 게임 시작 화면 출력
-    read_bmp("output.bmp", &data, &cols, &rows);
+    read_bmp("start.bmp", &data, &cols, &rows);
     fb_write(data, cols, rows);
     //usleep(3000000); // 3초 대기
     usleep(3); // 3초 대기
